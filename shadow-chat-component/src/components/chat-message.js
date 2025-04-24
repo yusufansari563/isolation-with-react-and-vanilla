16042dba7fdfbbe934ead775015722ddd526db1c
yusufansari <yusufansari563@gmail.com>
2025-04-24 18:11:07 +0530

    Update chat-message.js

--- shadow-chat-component/src/components/chat-message.js
+++ shadow-chat-component/src/components/chat-message.js
@@ -1,29 +1,28 @@
 export class ChatMessage extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-    }
-  
-    connectedCallback() {
-      const text = this.getAttribute('text') || 'No message';
-      this.render(text);
-    }
-  
-    render(text) {
-      this.shadowRoot.innerHTML = `
-        <style>
-          .message {
-            background: #f1f1f1;
-            padding: 5px 10px;
-            border-radius: 5px;
-            margin: 5px 0;
-            display: inline-block;
-          }
-        </style>
-        <div class="message">${text}</div>
-      `;
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
   }
-  
-  customElements.define('chat-message', ChatMessage);
 
+  connectedCallback() {
+    const text = this.getAttribute('text') || 'No message';
+    this.render(text);
+  }
+
+  render(text) {
+    this.shadowRoot.innerHTML = `
+      <style>
+        .inline-block {
+          background: #f1f1f1;
+          padding: 5px 10px;
+          border-radius: 5px;
+          margin: 5px 0;
+          display: inline-block;
+        }
+      </style>
+      <div class="inline-block">${text}</div>
+    `;
+  }
+}
+
+customElements.define('chat-message', ChatMessage);
